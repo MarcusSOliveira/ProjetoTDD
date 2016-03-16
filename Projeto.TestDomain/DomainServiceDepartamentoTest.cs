151f09583ad42b32ed84a49145e8cf3419205c93
Marcus Santos Oliveira <mso10br@gmail.com>
2016-03-16 12:35:49 -0300

    Added files via upload

---
+++ Projeto.TestDomain/DomainServiceDepartamentoTest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projeto.Dominio.DomainContracts.Services;
+using Projeto.Entidades;
+using Projeto.Dominio.DomainServices;
+using Projeto.Infra.Repositorio.Repository;
+
+namespace Projeto.TestDomain
+{
+    [TestClass]
+    public class DomainServiceDepartamentoTest
+    {
+        //atributo..
+        private IDomainServiceDepartamento dominio;
+
+        public DomainServiceDepartamentoTest()
+        {
+            dominio = new DomainServiceDepartamento(new RepositoryDepartamento());
+        }
+
+        [TestMethod]
+        public void TestCadastroDepartamento()
+        {
+            try
+            {
+                Departamento d = new Departamento()
+                {
+                    Nome = "Departamento Teste"
+                };
+
+                dominio.Cadastrar(d);                
+                Assert.IsTrue(d.IdDepartamento > 0);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestAlteracaoDepartamento()
+        {
+            try
+            {
+                Departamento d = new Departamento()
+                {
+                    Nome = "Departamento Teste"
+                };
+
+                dominio.Cadastrar(d); //gravando..
+                //buscar pelo id..
+                Departamento registro = dominio.ObterPorId(d.IdDepartamento);
+
+                //modificar os dados..
+                registro.Nome = "Departamento Teste Novo";
+
+                //alterar..
+                dominio.Alterar(registro);
+
+                //buscar na base de dados..
+                Departamento fAlteracao = dominio.ObterPorId(registro.IdDepartamento);
+                Assert.AreEqual(fAlteracao.Nome, registro.Nome);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestExclusaoDepartamento()
+        {
+            try
+            {
+                Departamento d = new Departamento()
+                {
+                    Nome = "Departamento Teste"
+                };
+
+                dominio.Cadastrar(d); //gravando.
+                dominio.Excluir(d.IdDepartamento); //excluindo..
+
+                Assert.IsNull(dominio.ObterPorId(d.IdDepartamento));
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+
+        [TestMethod]
+        public void TestObterDepartamento()
+        {
+            try
+            {
+                Departamento d = new Departamento()
+                {
+                    Nome = "Departamento Teste"
+                };
+
+                dominio.Cadastrar(d); //gravando..
+                //verificar se o funcionario foi encontrado pelo id..
+                Assert.IsNotNull(dominio.ObterPorId(d.IdDepartamento));
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestObterTodosDepartamento()
+        {
+            try
+            {
+                Assert.IsTrue(dominio.ObterTodos().Count > 0);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+    }
+}
---
+++ Projeto.TestDomain/DomainServiceFuncionarioTest.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projeto.Dominio.DomainContracts.Services;
+using Projeto.Entidades;
+using Projeto.Dominio.DomainServices;
+using Projeto.Infra.Repositorio.Repository;
+
+namespace Projeto.TestDomain
+{
+    [TestClass]
+    public class DomainServiceFuncionarioTest
+    {
+        //atributo..
+        private IDomainServiceFuncionario dominio;
+
+        public DomainServiceFuncionarioTest()
+        {
+            dominio = new DomainServiceFuncionario(new RepositoryFuncionario());
+        }
+
+        [TestMethod]
+        public void TestCadastroFuncionario()
+        {
+            try
+            {
+                Funcionario f = new Funcionario()
+                {
+                    Nome = "Funcionario Teste",
+                    Salario = 1000M,
+                    DataAdmissao = DateTime.Now,
+                    Departamento = new Departamento()
+                    {
+                        IdDepartamento = 1
+                    }
+                };
+
+                dominio.Cadastrar(f);
+
+                //verificando se o id do funcionario foi gerado..
+                Assert.IsTrue(f.IdFuncionario > 0);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestAlteracaoFuncionario()
+        {
+            try
+            {
+                Funcionario f = new Funcionario()
+                {
+                    Nome = "Funcionario Teste",
+                    Salario = 1000M,
+                    DataAdmissao = DateTime.Now,
+                    Departamento = new Departamento()
+                    {
+                        IdDepartamento = 1
+                    }
+                };
+
+                dominio.Cadastrar(f); //gravando..
+                //buscar pelo id..
+                Funcionario registro = dominio.ObterPorId(f.IdFuncionario);
+
+                //modificar os dados..
+                registro.Nome = "Funcionario Teste Novo";
+                registro.Salario = 3000M;
+
+                //alterar..
+                dominio.Alterar(registro);
+
+                //buscar na base de dados..
+                Funcionario fAlteracao = dominio.ObterPorId(registro.IdFuncionario);
+
+                Assert.AreEqual(fAlteracao.Nome, registro.Nome);
+                Assert.AreEqual(fAlteracao.Salario, registro.Salario);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestExclusaoFuncionario()
+        {
+            try
+            {
+                Funcionario f = new Funcionario()
+                {
+                    Nome = "Funcionario Teste",
+                    Salario = 1000M,
+                    DataAdmissao = DateTime.Now,
+                    Departamento = new Departamento()
+                    {
+                        IdDepartamento = 1
+                    }
+                };
+
+                dominio.Cadastrar(f); //gravando.
+                dominio.Excluir(f.IdFuncionario); //excluindo..
+
+                Assert.IsNull(dominio.ObterPorId(f.IdFuncionario));
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+
+        [TestMethod]
+        public void TestObterFuncionario()
+        {
+            try
+            {
+                Funcionario f = new Funcionario()
+                {
+                    Nome = "Funcionario Teste",
+                    Salario = 1000M,
+                    DataAdmissao = DateTime.Now,
+                    Departamento = new Departamento()
+                    {
+                        IdDepartamento = 1
+                    }
+                };
+
+                dominio.Cadastrar(f); //gravando..
+                //verificar se o funcionario foi encontrado pelo id..
+                Assert.IsNotNull(dominio.ObterPorId(f.IdFuncionario));
+            }
+            catch(Exception e)
+            {                
+                Assert.Fail(e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestObterTodosFuncionario()
+        {
+            try
+            {
+                Assert.IsTrue(dominio.ObterTodos().Count > 0);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+    }
+}
